101aa8ebe589d811858b16fe33d6f1c5af0605c3
Wholeheartedly <119653204+AESMatias@users.noreply.github.com>
2025-01-14 20:55:36 -0300

    Add winston logger and add /query endpoint

--- controllers/openaiController.ts
+++ controllers/openaiController.ts
@@ -20,7 +20,9 @@ export const queryOpenAI = async (req: Request, res: Response): Promise<void> =>
         res.status(400).json({ error: 'Prompt is required' });
         return;
     }
-
+    console.log('Prompt:', prompt)
+    console.log('Messages:', messages)
+    console.log('req completo:', req.body)
     try {
     const response = await client.chat.completions.create({
         messages: [
@@ -30,11 +32,7 @@ export const queryOpenAI = async (req: Request, res: Response): Promise<void> =>
                     a philosopher. You think you are Wittgenstein herself, designed for mathematicians,\
                     data science, engineering, statisticians and physics students. You do not talk\
                     more than necessary unless the user ask you to do it. Also, whatever the user ask, you do \
-                    without hesitation, you serve like an assistant.\
-                    Do you speak in Spanish unless the user spoke in Spanish aswell.\
-                    It is very much important you use TeX for any math question.\
-                    For any math question, you generate the response in TeX format.\
-                    If the question is not about math, format the response for Discord."
+                    without hesitation, you serve like an assistant"
                 },
                 ...messages
         ],
@@ -45,6 +43,7 @@ export const queryOpenAI = async (req: Request, res: Response): Promise<void> =>
     res.json({ result: response});
 
   } catch (error) {
+    console.log('Error at queryOpenAI:', error)
     res.status(500).json({ error: 'Error communicating with OpenAI' });
   }
 };
